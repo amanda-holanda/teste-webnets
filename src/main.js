77262331a3eea635ba9e25fd9af8110bf9b4e5da
Amanda Holanda <amandaholanda_@hotmail.com>
2023-03-09 23:13:10 -0300

    finalização das features

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 const darkModeButton = document.querySelector("#darkModeBtn");
-const body = document.querySelector("#body");
+const elementBody = document.querySelector("#body");
+const increaseFontButton = document.querySelector("#increaseFontBtn");
+const decreaseFontButton = document.querySelector("#decreaseFontBtn");
+const fontSize = 100;
+const increaseDecrease = 10;
+let tamanho = 16;
 
 darkModeButton.addEventListener("click", () => {
-  body.classList.toggle("dark-mode-option");
+  elementBody.classList.toggle("dark-mode-option");
+});
+
+function increaseFont() {
+  tamanho++;
+  elementBody.style.fontSize = tamanho + "px";
+}
+
+function decreaseFont() {
+  tamanho--;
+  elementBody.style.fontSize = tamanho + "px";
+}
+
+increaseFontButton.addEventListener("click", () => {
+  increaseFont();
+});
+
+decreaseFontButton.addEventListener("click", () => {
+  decreaseFont();
 });
